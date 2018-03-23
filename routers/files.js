f8f5da21da6055f4c58484179a0f2b4280c5039e
roy <roy.k.gao@gmail.com>
2018-03-23 17:34:58 +0800

    读取文件

--- routers/files.js
+++ routers/files.js
@@ -1,23 +1,30 @@
-const router = require('koa-router')()
-const path = require('path')
-const static = require('koa-static')
-const fs = require('fs')
-const { uploadFile } = require('../util/upload')
-const {savePath} = require('../config/savePath')
+const router = require('koa-router')();
+const path = require('path');
+const static = require('koa-static');
+const fs = require('fs');
 
-console.log('savePath', savePath, )
+const {uploadFile} = require('../util/upload');
+const {readDirName} = require('../util/files');
 
-module.exports = router.post('/upload/picture', async (ctx) => {
+const {savePath} = require('../config/savePath');
+
+// console.log('savePath', savePath,);
+
+module.exports = router.post('/upload/picture/:dir', async (ctx) => {
     // 上传文件请求处理
-    let result = { success: false }
-    let serverFilePath = path.join(__dirname, 'static/image')
+    // let result = {success: false};
+    // let serverFilePath = path.join(__dirname, 'static/image');
 
     // 上传文件事件
-    result = await uploadFile(ctx, {
-        fileType: 'images', // todo tag
-        path: savePath
-    })
-    ctx.body = result
+    ctx.body = await uploadFile(ctx, {
+        fileType: ctx.params.dir,
+        path: savePath,
+    });
+}).get('/getFileTag', async (ctx) => {
+    ctx.body = await readDirName(ctx, savePath);
+}).get('/getFiles/:dir', async (ctx) => {
+    const filePath = path.join(savePath, ctx.params.dir || 'other');
+    ctx.body = await readDirName(ctx, filePath);
 }).get('/helloworld', async (ctx) => {
-    ctx.body = 'helloworld page!' + savePath
-})
+    ctx.body = 'helloworld page!' + savePath;
+});
